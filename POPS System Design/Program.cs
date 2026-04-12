46d46f52497a028e5bf0431d82162962e58c0bee
Ali Cetin <cetin.2@wright.edu>
2026-04-12 03:57:03 -0400

    a

--- POPS System Design/Program.cs
+++ POPS System Design/Program.cs
@@ -16,31 +16,7 @@ namespace POPS_System_Design
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
             Application.Run(new MainFormSales());
-=======
-            Application.Run(new MainPortalPage());
->>>>>>> Stashed changes
-=======
-            Application.Run(new MainFormSales());
->>>>>>> Stashed changes
-=======
-            Application.Run(new MainFormSales());
->>>>>>> Stashed changes
-=======
-            Application.Run(new MainFormSales());
->>>>>>> Stashed changes
-=======
-            Application.Run(new MainFormSales());
->>>>>>> Stashed changes
-=======
-            Application.Run(new MainFormSales());
->>>>>>> Stashed changes
         }
     }
 }
