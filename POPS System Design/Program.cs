a83b06b5d19bee711940f9ecc1090eb532b0a10f
Ali Cetin <cetin.2@wright.edu>
2026-04-12 03:49:44 -0400

    a

--- POPS System Design/Program.cs
+++ POPS System Design/Program.cs
@@ -20,9 +20,13 @@ namespace POPS_System_Design
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             Application.Run(new MainFormSales());
 =======
             Application.Run(new MainPortalPage());
+>>>>>>> Stashed changes
+=======
+            Application.Run(new MainFormSales());
 >>>>>>> Stashed changes
 =======
             Application.Run(new MainFormSales());
